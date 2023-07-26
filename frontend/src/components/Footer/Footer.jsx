5031e125e22b1a73ab9f59f4ecc77cb05d51ffc6
DG15062002 <83827419+DG15062002@users.noreply.github.com>
2023-07-26 13:22:59 +0530

    Create Footer.jsx

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+// jshint esversion: 10
+import styles from './Footer.module.css';
+
+function Footer() {
+    return (
+        <p className={styles.footer}>&copy; CoinBounce 2023</p>
+    )
+}
+
+export default Footer;
